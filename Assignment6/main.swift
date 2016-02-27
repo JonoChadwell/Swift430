a1e3c36162fc58f741bb259b1f648ebaff92493a
JonoChadwell <jono.chadwell@gmail.com>
2016-02-27 13:51:04 -0800

    Removed Interp

--- Assignment6/main.swift
+++ Assignment6/main.swift
@@ -8,8 +8,8 @@ print("This is a test")
 class ValueV {}
 class NumV : ValueV {
     let val : Int
-    init(_val : Int) {
-        val = _val
+    init(_ val : Int) {
+        self.val = val
     }
 }
 class TrueV : ValueV {}
@@ -18,34 +18,32 @@ class FalseV : ValueV {}
 
 // Expressions
 class ExprC {
-    
+    func evaluate() -> ValueV {
+        preconditionFailure("We are pretending this is abstract");
+    }
 }
 class LiteralC : ExprC {
     let val : ValueV
-    init(_val : ValueV) {
-        val = _val
+    init(_ val : ValueV) {
+        self.val = val
+    }
+    override func evaluate() -> ValueV {
+        return val
     }
 }
+
 class PlusC : ExprC {
     let left : ExprC
     let right : ExprC
-    init(_left : ExprC, _right : ExprC) {
-        left = _left
-        right = _right
+    init(_ left : ExprC, _ right : ExprC) {
+        self.left = left
+        self.right = right
+    }
+    override func evaluate() -> ValueV {
+        return NumV((left.evaluate() as! NumV).val + (right.evaluate() as! NumV).val)
     }
 }
 
-func Interp(exp : ExprC)->ValueV {
-    if ((exp as? LiteralC) != nil) {
-        return (exp as! LiteralC).val
-    } else if ((exp as? PlusC) != nil) {
-        let expP = exp as! PlusC
-        return NumV(_val:(Interp(expP.left) as! NumV).val + (Interp(expP.right) as! NumV).val)
-    } else {
-        return FalseV()
-    }
-}
+let thing = PlusC(LiteralC(NumV(4)), LiteralC(NumV(5)))
 
-let thing = PlusC(_left:LiteralC(_val:NumV(_val:4)), _right:LiteralC(_val:NumV(_val:5)))
-
-print((Interp(thing) as! NumV).val)
+print((thing.evaluate() as! NumV).val)
