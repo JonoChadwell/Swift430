e82c2ccf0410c9f0ab8d6c6f981c72c2ae15a4c9
JonoChadwell <jono.chadwell@gmail.com>
2016-02-27 12:44:52 -0800

    Initial Commit

---
+++ Assignment6/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Assignment6
+//
+//  Created by Jonathan Chadwell on 2/27/16.
+//  Copyright © 2016 Jonathan Chadwell. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
