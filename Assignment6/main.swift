366f717ebea5c49f0467ed4d0af281468ee5391f
JonoChadwell <jono.chadwell@gmail.com>
2016-02-27 13:14:18 -0800

    Values and ExprC type

--- Assignment6/main.swift
+++ Assignment6/main.swift
@@ -1,12 +1,28 @@
-//
-//  main.swift
-//  Assignment6
-//
-//  Created by Jonathan Chadwell on 2/27/16.
-//  Copyright © 2016 Jonathan Chadwell. All rights reserved.
-//
-
 import Foundation
 
 print("Hello, World!")
 
+print("This is a test")
+
+// Values
+class ValueV {}
+class NumV : ValueV {
+    let val : Int
+    init(_val : Int) {
+        val = _val
+    }
+}
+class TrueV : ValueV {}
+class FalseV : ValueV {}
+
+
+// Expressions
+class ExprC {
+    
+}
+class LiteralC : ExprC {
+    let val : ValueV
+    init(_val : ValueV) {
+        val = _val
+    }
+}
