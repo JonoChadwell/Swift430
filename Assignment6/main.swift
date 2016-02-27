da57cb6e582b9e071e9c8a2f2ff14293baba16bc
JonoChadwell <jono.chadwell@gmail.com>
2016-02-27 13:31:12 -0800

    Added Interp Function

--- Assignment6/main.swift
+++ Assignment6/main.swift
@@ -26,3 +26,26 @@ class LiteralC : ExprC {
         val = _val
     }
 }
+class PlusC : ExprC {
+    let left : ExprC
+    let right : ExprC
+    init(_left : ExprC, _right : ExprC) {
+        left = _left
+        right = _right
+    }
+}
+
+func Interp(exp : ExprC)->ValueV {
+    if ((exp as? LiteralC) != nil) {
+        return (exp as! LiteralC).val
+    } else if ((exp as? PlusC) != nil) {
+        let expP = exp as! PlusC
+        return NumV(_val:(Interp(expP.left) as! NumV).val + (Interp(expP.right) as! NumV).val)
+    } else {
+        return FalseV()
+    }
+}
+
+let thing = PlusC(_left:LiteralC(_val:NumV(_val:4)), _right:LiteralC(_val:NumV(_val:5)))
+
+print((Interp(thing) as! NumV).val)
